4aeab5ba70f31fb76ec7aa51e3bab6424f68a1ec
Mike Stuart <mikestuart11@gmail.com>
2015-09-30 12:12:03 -0600

    add folders and new urlConnectionClass.java

--- src/urlConnectionClass.java
+++ src/urlConnectionClass.java
@@ -1,5 +1,28 @@
+import java.io.*;
+import java.net.*;
+
 /**
  * Created by Mike on 9/29/2015.
+ * I'm not quite understanding this. I think there is a problem with the readStream() method. This is incomplete and
+ * I'll go figure out more of what it does and how it works.
  */
 public class urlConnectionClass {
+
+    public static void main(String args[]) {
+
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL("http://www.android.com/");
+            urlConnection = (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            readStream(in);
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 }
