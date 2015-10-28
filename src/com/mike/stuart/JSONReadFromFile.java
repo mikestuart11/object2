0e21d98a9f52b14c666eadba523e70d243d8f19c
Mike Stuart <mikestuart11@gmail.com>
2015-10-28 13:46:12 -0600

    update all JSON files

--- src/com/mike/stuart/JSONReadFromFile.java
+++ src/com/mike/stuart/JSONReadFromFile.java
@@ -3,6 +3,7 @@ package com.mike.stuart;
 import org.quickconnectfamily.json.JSONException;
 import org.quickconnectfamily.json.JSONInputStream;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
@@ -19,13 +20,12 @@ public class JSONReadFromFile {
          * This should read from a file called test2.txt and inside this file there is a personBean object in JSON
          * format. This should read that information and then parse it into strings so it can be used to make a
          * personBean object.
-         *
          **********************************************************************************************************/
 
-        String fileName = "test2.txt";
-        System.out.println("Happy Path:");
-        // Happy Path
-        // this reads in the file and turns it into a personBean
+        String fileName = "test.json";
+        System.out.println("Happy Path 1 :");
+        // Happy Path --------------------------------------------------------------------------------------------------
+        // this reads in the file and saves the information to a hashMap
         try {
             FileInputStream objectFileStream = new FileInputStream(fileName);
             JSONInputStream jsonIn = new JSONInputStream(objectFileStream);
@@ -36,38 +36,35 @@ public class JSONReadFromFile {
             e.printStackTrace();
         }
 
-        // Nasty Path 1
-        // This is if there is no file input into the file stream, the file is null
-        System.out.println("Nasty Path 1: ");
-        String nullFileName = null;
+//        // Nasty Path 1 ----------------------------------------------------------------------------------------------
+//        // Putting a null filename into the file input stream
+//        System.out.println("Nasty Path 1: ");
+//        String nullFileName = null;
+//        try {
+//            FileInputStream objectFileStream = new FileInputStream(nullFileName);
+//            JSONInputStream jsonIn = new JSONInputStream(objectFileStream);
+//            HashMap test2HashMap = (HashMap) jsonIn.readObject();
+//            System.out.println("Output: " + test2HashMap);
+//            jsonIn.close();
+//        } catch (JSONException|IOException e) {
+//            e.printStackTrace();
+//        } // this passes errors
 
-        try {
-            FileInputStream objectFileStream = new FileInputStream(nullFileName);
-            JSONInputStream jsonIn = new JSONInputStream(objectFileStream);
-            HashMap test2HashMap = (HashMap) jsonIn.readObject();
+//        // Nasty Path 2 ------------------------------------------------------------------------------------------------
+//        // if a null is put into the jsoninputstream
+//        System.out.println("Nasty Path 2: ");
+//        try {
+//            FileInputStream objectFileStream = new FileInputStream(fileName);
+//            JSONInputStream jsonIn = new JSONInputStream(null);
+//            HashMap test3HashMap = (HashMap) jsonIn.readObject();
+//
+//            System.out.println("Output: " + test3HashMap);
+//            jsonIn.close();
+//        } catch (JSONException|IOException e) {
+//            e.printStackTrace();
+//        } // this passes errors
 
-            System.out.println("Output: " + test2HashMap);
-            jsonIn.close();
-        } catch (JSONException|IOException e) {
-            e.printStackTrace();
-        }
-
-        // Nasty Path 2
-        // if a null is put into the jsoninputstream
-        System.out.println("Nasty Path 2: ");
-
-        try {
-            FileInputStream objectFileStream = new FileInputStream(fileName);
-            JSONInputStream jsonIn = new JSONInputStream(null);
-            HashMap test3HashMap = (HashMap) jsonIn.readObject();
-
-            System.out.println("Output: " + test3HashMap);
-            jsonIn.close();
-        } catch (JSONException|IOException e) {
-            e.printStackTrace();
-        }
-
-        // Nasty Path 3
+        // Nasty Path 3 ------------------------------------------------------------------------------------------------
         // put in a negative int into jsoninputstream
         System.out.println("Nasty Path 3: ");
         String number = "-1";
@@ -80,25 +77,50 @@ public class JSONReadFromFile {
             jsonIn.close();
         } catch (JSONException|IOException e) {
             e.printStackTrace();
-        }
+        } //
 
-        // Nasty Path 4
-        // put in a negative int into jsoninputstream
+        // Nasty Path 4 ------------------------------------------------------------------------------------------------
+        // reading from a file that isn't a json file
         System.out.println("Nasty Path 4: ");
-        String fileName2 = "test2.json";
+        String imageTestFile = "imageJsonTest.bmp";
         try {
-            FileInputStream objectFileStream = new FileInputStream(fileName2);
+            FileInputStream objectFileStream = new FileInputStream(imageTestFile);
             JSONInputStream jsonIn = new JSONInputStream(objectFileStream);
             HashMap test1HashMap = (HashMap)jsonIn.readObject();
             System.out.println("Output :" + test1HashMap);
             jsonIn.close();
         } catch (JSONException|IOException e) {
             e.printStackTrace();
-        }
+        } // this passes the error: Invalid JSON String
 
-// read from a file that doesn't exist
-        // read from a file that isn't a json file
-        // read from a file and you don't have read permissions
+        // Nasty Path 5 ------------------------------------------------------------------------------------------------
+        // reading from a file that doesn't exist
+        System.out.println("Nasty Path 5: ");
+        String fileThatDoesntExist = "fileThatDoesntExist.json";
+        try {
+            FileInputStream objectFileStream = new FileInputStream(fileThatDoesntExist);
+            JSONInputStream jsonIn = new JSONInputStream(objectFileStream);
+            HashMap test1HashMap = (HashMap)jsonIn.readObject();
+            System.out.println("Output :" + test1HashMap);
+            jsonIn.close();
+        } catch (JSONException|IOException e) {
+            e.printStackTrace();
+        } // Passes these errors: Invalid JSON string and The system cannot find the file specified
+
+        // Nasty Path 6 ------------------------------------------------------------------------------------------------
+        // reading from a file that we don't have permissions to
+        System.out.println("Nasty Path 6: ");
+        String noPermissionsFile = "C:/Users/Mike/Object2/testFolder/dontHavePermissionsFile.txt";
+        try {
+            FileInputStream objectFileStream = new FileInputStream(noPermissionsFile);
+            JSONInputStream jsonIn = new JSONInputStream(objectFileStream);
+            HashMap test1HashMap = (HashMap)jsonIn.readObject();
+            System.out.println("Output :" + test1HashMap);
+            jsonIn.close();
+        } catch (JSONException|IOException e) {
+            e.printStackTrace();
+        } // Passes these errors: Invalid JSON string and The system cannot find the file specified
+
 
     }
 }
--- src/com/mike/stuart/JSONStringToObject.java
+++ src/com/mike/stuart/JSONStringToObject.java
@@ -19,19 +19,12 @@ public class JSONStringToObject {
         // Happy Path --------------------------------------------------------------------------------------------------
         System.out.println("Happy Path: ");
         try {
-            // this creates a hashmap and parses the JSON string into it
             HashMap aMap = (HashMap) JSONUtilities.parse(jsonString);
-            // this prints out the hashmap to the console, you'll notice that it's out of order
-            // this is because hashmaps don't care what order things are in
             System.out.println("Output: " + aMap);
-            // this gets the values from the hashmap and parses them
-            // into variables to later be put into a bean
             String firstName = (String) aMap.get("theFirstName");
             String lastName = (String) aMap.get("theLastName");
             int age = Integer.parseInt((String) aMap.get("theAge"));
-            // I take the parsed strings and ints and put them into a bean
             personBean newBean3 = new personBean(firstName, lastName, age);
-            // I create a file to send all of the information to
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -41,19 +34,12 @@ public class JSONStringToObject {
         // put a null into the JSONUtility function
         System.out.println("Nasty Path 1: ");
         try {
-            // this creates a hashmap and parses the JSON string into it
             HashMap aMap = (HashMap) JSONUtilities.parse(null);
-            // this prints out the hashmap to the console, you'll notice that it's out of order
-            // this is because hashmaps don't care what order things are in
             System.out.println("Output: " + aMap);
-            // this gets the values from the hashmap and parses them
-            // into variables to later be put into a bean
             String firstName = (String) aMap.get("theFirstName");
             String lastName = (String) aMap.get("theLastName");
             int age = Integer.parseInt((String) aMap.get("theAge"));
-            // I take the parsed strings and ints and put them into a bean
             personBean newBean2 = new personBean(firstName, lastName, age);
-            // I create a file to send all of the information to
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -63,19 +49,12 @@ public class JSONStringToObject {
         // put a -1 string in for the JSON Utilities function
         System.out.println("Nasty Path 2: ");
         try {
-            // this creates a hashmap and parses the JSON string into it
             HashMap aMap = (HashMap) JSONUtilities.parse("-1");
-            // this prints out the hashmap to the console, you'll notice that it's out of order
-            // this is because hashmaps don't care what order things are in
             System.out.println("Output: " + aMap);
-            // this gets the values from the hashmap and parses them
-            // into variables to later be put into a bean
             String firstName = (String) aMap.get("theFirstName");
             String lastName = (String) aMap.get("theLastName");
             int age = Integer.parseInt((String) aMap.get("theAge"));
-            // I take the parsed strings and ints and put them into a bean
             personBean newBean2 = new personBean(firstName, lastName, age);
-            // I create a file to send all of the information to
 
         } catch (JSONException e) {
             e.printStackTrace();
