12f42095255741aa2e435f245bcdd97e8cd38b8c
Mike Stuart <mikestuart11@gmail.com>
2015-10-29 12:34:43 -0600

    update treeMapSandbox with Nasty Paths

--- src/com/mike/stuart/Collections/treeMapSandbox.java
+++ src/com/mike/stuart/Collections/treeMapSandbox.java
@@ -1,48 +1,68 @@
 package com.mike.stuart.Collections;
-
 import java.util.*;
 
 public class treeMapSandbox {
-
     public static void main(String args[]) {
 
         // Happy path --------------------------------------------------------------------------------------------------
         // create treeMap and add values to it
         System.out.println("Happy Path 1: ");
-        TreeMap<String, String> tm = new TreeMap<String, String>();
+        TreeMap<String, String> treeMapExample = new TreeMap<String, String>();
 
-        tm.put("Mike Stuart", "Vice Principal");
-        tm.put("Madison Stuart", "Principal");
+        treeMapExample.put("Mike Stuart", "Vice Principal");
+        treeMapExample.put("Madison Stuart", "Principal");
 
-        System.out.println(tm);
+        System.out.println(treeMapExample);
         // this prints out the treeMap as expected
 
-        // Nasty path --------------------------------------------------------------------------------------------------
+        // Nasty path 1 ------------------------------------------------------------------------------------------------
         // adds a null value associated with a string key
         System.out.println("Nasty Path 1: ");
-        tm.put("Mike", null);
-        System.out.println(tm);
+        treeMapExample.put("Mike", null);
+        System.out.println(treeMapExample);
         // this prints out the treeMap and has the Mike key and the value is null
 
-        // nasty path --------------------------------------------------------------------------------------------------
-        // putting null in for a key
+        // Nasty path 2 ------------------------------------------------------------------------------------------------
+        // adds a null value into the putAll method
         System.out.println("Nasty Path 2: ");
-//        try {
-//            String nullString = null;
-//            tm.put(nullString, (d);
-//            System.out.println(tm);
-//        } catch (Exception e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-////
-////        // nasty path, using get for a key that doesn't exist
-////        try {
-////            Double result = tm.get("NotAName");
-////            System.out.println(result);
-////        } catch (Exception e) {
-////            e.printStackTrace();
-////        }
+        try {
+            treeMapExample.putAll(null);
+            System.out.println(treeMapExample);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } // Error: Error with Native Method, it didn't work
+
+        // nasty path 3 ------------------------------------------------------------------------------------------------
+        // putting a null in the a key
+        System.out.println("Nasty Path 3: ");
+        try {
+            String nullString = null;
+            treeMapExample.put(nullString, "Value");
+            System.out.println(treeMapExample);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } // Error: NullPointerException, it didn't work
+
+
+        // nasty path 4 ------------------------------------------------------------------------------------------------
+        // remove null from the treeMap
+        System.out.println("Nasty Path 4: ");
+        try {
+            treeMapExample.remove(null);
+            System.out.println(treeMapExample);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } // Error: NullPointerException
+
+        // nasty path 5 ------------------------------------------------------------------------------------------------
+        // remove null from the treeMap
+        System.out.println("Nasty Path 5: ");
+        try {
+
+            System.out.println(treeMapExample);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } // 
 
     }
 }
