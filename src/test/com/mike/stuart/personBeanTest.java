0b3f85b76b39f1aab86475ec825d6e75174576ec
Mike Stuart <mikestuart11@gmail.com>
2015-11-09 12:32:50 -0700

    add JUnit plugin and started personBean test

---
+++ src/test/com/mike/stuart/personBeanTest.java
@@ -0,0 +1,95 @@
+package test.com.mike.stuart; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* personBean Tester. 
+* 
+* @author <Mike Stuart>
+* @since <pre>Nov 9, 2015</pre> 
+* @version 1.0 
+*/ 
+public class personBeanTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getFirstName() 
+* 
+*/ 
+@Test
+public void testGetFirstName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setFirstName(String firstName) 
+* 
+*/ 
+@Test
+public void testSetFirstName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getLastName() 
+* 
+*/ 
+@Test
+public void testGetLastName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setLastName(String lastName) 
+* 
+*/ 
+@Test
+public void testSetLastName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getAge() 
+* 
+*/ 
+@Test
+public void testGetAge() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setAge(int age) 
+* 
+*/ 
+@Test
+public void testSetAge() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: toString() 
+* 
+*/ 
+@Test
+public void testToString() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
