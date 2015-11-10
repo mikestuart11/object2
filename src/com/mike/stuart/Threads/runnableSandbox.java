2c994fa89d6b59d56c6fd064efb11d7e54505064
Mike Stuart <mikestuart11@gmail.com>
2015-11-10 12:31:30 -0700

    add code to threads

---
+++ src/com/mike/stuart/Threads/runnableSandbox.java
@@ -0,0 +1,17 @@
+package com.mike.stuart.Threads;
+
+/**
+ * Created by Mike on 11/10/2015.
+ */
+public class runnableSandbox implements Runnable {
+    public void run() {
+        for (int i = 0; i < 3; i++) {
+            try {
+                System.out.println("RunnableSandbox is running.");
+                Thread.currentThread().sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
